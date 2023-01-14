25223b607cfd579d13cf3c2784c6fac6fa8e9ec1
ClaudiaCristinaTomus <tomus_clau@yahoo.com>
2023-01-15 00:46:35 +0200

    add files

---
+++ src/components/FormInput.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./formInputs.css"
+import { useState } from 'react';
+
+export const FormInput = (props) => {
+  const[focused,setFocused]=useState(false);
+  const{label,errorMessage,onChange,id,...inputProps}=props;
+
+  const handleFocus = (e)=>{
+    setFocused(true);
+  };
+
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()}/>
+      <span>{errorMessage}</span>
+    </div>
+  )
+}
+
+export default FormInput;
